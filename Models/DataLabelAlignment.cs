bcb1d913c2f42b317633ac4faa17011ffff91311
Dávid El-Saig <david.el-saig@lombiq.com>
2021-03-25 20:24:22 +0100

    Added docs.

--- Models/DataLabelAlignment.cs
+++ Models/DataLabelAlignment.cs
@@ -1,5 +1,11 @@
 namespace Lombiq.ChartJs.Models
 {
+    /// <summary>
+    /// Identifies where to put the data label in relation to the chart data visualizer (eg. point or bar). The default
+    /// value in the library is <see cref="Center"/> <see
+    /// href="https://chartjs-plugin-datalabels.netlify.app/guide/positioning.html#alignment-and-offset)"> (see docs
+    /// here)</see> which is why it's the first member.
+    /// </summary>
     public enum DataLabelAlignment
     {
         Center,
