012942b41db9945424c83a339804134e7732a4a4
Márk Bartha <mark.bartha@lombiq.com>
2021-09-15 19:29:08 +0200

    Removing reference to non-existing Assets folder

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,6 @@
 const gulp = require('gulp');
 const paths = require('./Gulp/paths');
-const jsTargets = require('../../Utilities/Lombiq.Gulp.Extensions/Tasks/js-targets');
 const copyAssets = require('./Gulp/tasks/copy-assets');
 
 gulp.task('copy:vendor-assets', () => copyAssets(paths.vendorAssets, paths.dist.vendors));
-gulp.task('build:lombiq-js', () => jsTargets.compile(paths.lombiqAssets.base, paths.dist.lombiq));
-gulp.task('default', gulp.parallel('copy:vendor-assets', 'build:lombiq-js'));
+gulp.task('default', gulp.parallel('copy:vendor-assets'));
