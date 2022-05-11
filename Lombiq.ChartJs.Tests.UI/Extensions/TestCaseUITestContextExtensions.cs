4620b90f1fc836e25e2857289ecba343d1ca363f
József Horváth <info@ministro.hu>
2022-05-11 23:49:44 +0200

    Fixing comments

--- Lombiq.ChartJs.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.ChartJs.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -12,18 +12,17 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
-// We can't import the whole System.Drawing namespace because both
-// of SixLabors.ImageSharp and System.Drawing are contains Image class,
-// but we want it and some other from SixLabors.ImageSharp.
+
+// We can't import the whole System.Drawing namespace because both of SixLabors.ImageSharp and System.Drawing are
+// contains Image class, but we want it and some other from SixLabors.ImageSharp.
 // So we import only Bitmap from System.Drawing here.
 using Bitmap = System.Drawing.Bitmap;
 
-// The SixLabors.ImageSharp.Web[2.0.0] which is depends on SixLabors.ImageSharp[2.1.1]
-//  will be introduced in Orchard in the future.
-//  see: https://github.com/OrchardCMS/OrchardCore/pull/11585
-// When it will be done and we change the new version of OC in Lombiq.ChartJs.Samples,
-//  we should change from ImageSharpCompare[1.2.11]
-//  to Codeuctivity.ImageSharpCompare[2.0.46] in this project
+// The SixLabors.ImageSharp.Web v2.0.0 which is depends on SixLabors.ImageSharp v2.1.1 will be introduced in Orchard
+// in the future.
+// See: https://github.com/OrchardCMS/OrchardCore/pull/11585
+// When it will be done and we change the new version of OC in Lombiq.ChartJs.Samples, we should change from
+// ImageSharpCompare v1.2.11 to Codeuctivity.ImageSharpCompare v2.0.46 in this project
 
 namespace Lombiq.ChartJs.Tests.UI.Extensions;
 
