937a8d892ee8ea2491ec8c144ae0fdf6d6ddfc98
József Horváth <info@ministro.hu>
2022-05-24 22:02:29 +0200

    changing to image comparison and screen capture extensions moved to Lombiq.UITestingToolbox

--- Lombiq.ChartJs.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.ChartJs.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -1,28 +1,11 @@
 using Atata;
-using Codeuctivity;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
 using Shouldly;
 using SixLabors.ImageSharp;
 using System;
-using System.Drawing.Imaging;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
-
-// We can't import the whole System.Drawing namespace because both of SixLabors.ImageSharp and System.Drawing are
-// contains Image class, but we want it and some other from SixLabors.ImageSharp.
-// So we import only Bitmap from System.Drawing here.
-using Bitmap = System.Drawing.Bitmap;
-
-// The SixLabors.ImageSharp.Web v2.0.0 which is depends on SixLabors.ImageSharp v2.1.1 will be introduced in Orchard
-// in the future.
-// See: https://github.com/OrchardCMS/OrchardCore/pull/11585
-// When it will be done and we change the new version of OC in Lombiq.ChartJs.Samples, we should change from
-// ImageSharpCompare v1.2.11 to Codeuctivity.ImageSharpCompare v2.0.46 in this project
 
 namespace Lombiq.ChartJs.Tests.UI.Extensions;
 
@@ -56,15 +39,17 @@ public static class TestCaseUITestContextExtensions
         string referenceResourceName,
         double pixelErrorThreshold)
     {
+        var canvasElementSelector = By.TagName("canvas");
         // This is to avoid Chart.js animation related issues
-        var hash = context.WaitChartJsCanvasToBeReadyAndHash();
+        var hash = context.DoWaitElementToBeReady(
+            canvasElementSelector.Safely(),
+            TimeSpan.FromSeconds(30),
+            TimeSpan.FromMilliseconds(100));
         hash.ShouldNotBeNullOrEmpty();
 
         context.Scope.AtataContext.Log.Trace("{0}: imageHash: {1}", logHeader, hash);
 
-        var canvas = context.Driver.FindElementByTagName("canvas");
-        canvas.ShouldNotBeNull();
-        using var canvasImage = context.TakeScreenshotImage(canvas)
+        using var canvasImage = context.TakeElementScreenshot(canvasElementSelector)
             .ToImageSharpImage();
         canvasImage.ShouldNotBeNull()
             .SaveAsBmp($"Temp/{logHeader}_canvas.bmp");
@@ -73,15 +58,13 @@ public static class TestCaseUITestContextExtensions
         referenceImage.ShouldNotBeNull()
             .SaveAsBmp($"Temp/{logHeader}_reference.bmp");
 
-        using var diffImage = ImageSharpCompare.CalcDiffMaskImage(
-            $"Temp/{logHeader}_canvas.bmp",
-            $"Temp/{logHeader}_reference.bmp");
+        using var diffImage = referenceImage
+            .CalcDiffImage(canvasImage);
         diffImage.ShouldNotBeNull()
             .SaveAsBmp($"Temp/{logHeader}_diff.bmp");
 
-        var diff = ImageSharpCompare.CalcDiff(
-            $"Temp/{logHeader}_canvas.bmp",
-            $"Temp/{logHeader}_reference.bmp");
+        var diff = referenceImage
+            .CompareTo(canvasImage);
         context.Scope.AtataContext.Log.Trace(
             @"{0}: calculated differences:
     absoluteError={1},
@@ -98,62 +81,11 @@ public static class TestCaseUITestContextExtensions
         diff.PixelErrorPercentage.ShouldBeLessThan(pixelErrorThreshold);
     }
 
-    private static string WaitChartJsCanvasToBeReadyAndHash(
-        this UITestContext context,
-        double timeoutSec = 30,
-        double pollMillisec = 100)
-    {
-        string lastHash = null;
-        context.DoWithRetriesOrFail(
-            () =>
-            {
-                var canvas = context.Get(By.TagName("canvas").Safely());
-                var hash = context.ComputeElementImageHash(canvas);
-
-                var ready = hash == lastHash;
-
-                lastHash = hash;
-
-                return ready;
-            },
-            TimeSpan.FromSeconds(timeoutSec),
-            TimeSpan.FromMilliseconds(pollMillisec));
-        return lastHash;
-    }
-
-    private static string ComputeElementImageHash(this UITestContext context, IWebElement element)
-    {
-        using var elementImage = context.TakeScreenshotImage(element);
-        using var elementImageStream = new MemoryStream();
-
-        elementImage.Save(elementImageStream, ImageFormat.Bmp);
-        var elementImageRaw = elementImageStream.ToArray();
-        return ComputeSha256Hash(elementImageRaw);
-    }
-
-    private static string ComputeSha256Hash(byte[] raw)
-    {
-        using var sha256Hash = SHA256.Create();
-
-        return string.Concat(
-            sha256Hash.ComputeHash(raw)
-                .Select(item => item.ToString("x2", CultureInfo.InvariantCulture)));
-    }
-
     private static Image GetResourceImageSharpImage(string name)
     {
         using var resourceStream = typeof(TestCaseUITestContextExtensions)
             .Assembly
             .GetManifestResourceStream(name);
         return Image.Load(resourceStream);
-    }
-
-    private static Image ToImageSharpImage(this Bitmap bitmap)
-    {
-        using var memoryStream = new MemoryStream();
-        bitmap.Save(memoryStream, ImageFormat.Bmp);
-        memoryStream.Seek(0, SeekOrigin.Begin);
-
-        return Image.Load(memoryStream);
     }
 }
