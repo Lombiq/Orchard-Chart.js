254a64939b85f4c2e6722fd173c76c6b9df1fc0b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-06-02 16:30:07 +0200

    Fixing "S2094 Remove this empty class, write its code or make it an "interface"." analyzer violations

--- Lombiq.ChartJs.Samples/Models/IncomePart.cs
+++ Lombiq.ChartJs.Samples/Models/IncomePart.cs
@@ -1,5 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Lombiq.ChartJs.Samples.Models;
 
+[SuppressMessage(
+    "Minor Code Smell",
+    "S2094:Classes should not be empty",
+    Justification = "It's a dummy sample.")]
 public class IncomePart : TransactionPart
 {
 }
