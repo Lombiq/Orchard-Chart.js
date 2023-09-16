859807d014b8485f0d679943ff2f9841f274a3ee
Roland Márkus <roland.markus@lombiq.com>
2023-09-16 21:10:27 +0200

    Wording.

--- Lombiq.ChartJs.Samples/Controllers/SampleController.cs
+++ Lombiq.ChartJs.Samples/Controllers/SampleController.cs
@@ -184,7 +184,7 @@ public class SampleController : Controller
                         Date = dateIndex.Date!.Value,
                         Amount = numericIndex.Numeric ?? 0,
                     })
-            // Adding DateTimeKind requires an overload that needs 3 more parameters which seems to be overkill for our usecase.
+            // Adding DateTimeKind requires an overload that needs 3 more parameters which seems to be overkill for our purposes.
 #pragma warning disable S6562 // Always set the "DateTimeKind" when creating new "DateTime" instances
             .GroupBy(transaction => new DateTime(transaction.Date.Year, transaction.Date.Month, 1))
 #pragma warning restore S6562 // Always set the "DateTimeKind" when creating new "DateTime" instances
