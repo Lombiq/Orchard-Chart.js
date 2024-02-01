f942f6f1a78bf8eb306958232872c399ef514097
Roland Márkus <roland.markus@lombiq.com>
2024-02-01 11:26:52 +0100

    Resolving warnings.

--- Lombiq.ChartJs.Samples/Models/ExpensePart.cs
+++ Lombiq.ChartJs.Samples/Models/ExpensePart.cs
@@ -1,5 +1,7 @@
 namespace Lombiq.ChartJs.Samples.Models;
 
+#pragma warning disable S2094 // Classes should not be empty
 public class ExpensePart : TransactionPart
+#pragma warning restore S2094 // Classes should not be empty
 {
 }
