dd8f5313afaeefd2bfa077d613553243044485ca
Roland Márkus <roland.markus@lombiq.com>
2024-02-05 15:11:49 +0100

    Removing supresses.

--- Lombiq.ChartJs.Samples/Models/ExpensePart.cs
+++ Lombiq.ChartJs.Samples/Models/ExpensePart.cs
@@ -1,7 +1,5 @@
 namespace Lombiq.ChartJs.Samples.Models;
 
-#pragma warning disable S2094 // Classes should not be empty
 public class ExpensePart : TransactionPart
-#pragma warning restore S2094 // Classes should not be empty
 {
 }
