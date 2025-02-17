498314f2f501b6d47e800d09c8f21d5f41f653c3
Sára El-Saig <sara.el-saig@lombiq.com>
2025-02-17 11:23:54 +0100

    spell-check-ignore-line

--- Lombiq.ChartJs/ResourceManagementOptionsConfiguration.cs
+++ Lombiq.ChartJs/ResourceManagementOptionsConfiguration.cs
@@ -18,7 +18,7 @@ public partial class ResourceManagementOptionsConfiguration : IConfigureOptions<
     {
         _manifest
             .DefineScript(Library)
-            .SetUrl(Vendors + "chart.js/chart.umd.js")
+            .SetUrl(Vendors + "chart.js/chart.umd.js") // #spell-check-ignore-line
             .SetVersion(ChartJsVersion);
 
         _manifest
