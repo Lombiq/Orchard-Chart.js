a5a08d63d9ce55714ce57cb5708d08ffc7b26404
Sára El-Saig <sara.el-saig@lombiq.com>
2025-11-02 21:42:42 +0100

    Fix chartjs maybe.

--- Lombiq.ChartJs/ResourceManagementOptionsConfiguration.cs
+++ Lombiq.ChartJs/ResourceManagementOptionsConfiguration.cs
@@ -16,21 +16,21 @@ public partial class ResourceManagementOptionsConfiguration : IConfigureOptions<
     {
         _manifest
             .DefineScript(Library)
-            .SetUrl(Vendors + "chart.js/chart.umd.min.js", Vendors + "chart.js/chart.umd.js")
+            .SetUrl(Vendors + "chart.js/dist/chart.umd.min.js", Vendors + "chart.js/dist/chart.umd.js")
             .SetVersion(LibManVersions.ChartJs);
 
         _manifest
             .DefineScript(Annotation)
             .SetDependencies(Library)
-            .SetUrl(Vendors + "chartjs-plugin-annotation/chartjs-plugin-annotation.min.js")
+            .SetUrl(Vendors + "chartjs-plugin-annotation/dist/chartjs-plugin-annotation.min.js")
             .SetVersion(LibManVersions.ChartjsPluginAnnotation);
 
         _manifest
             .DefineScript(DataLabels)
             .SetDependencies(Library)
             .SetUrl(
-                Vendors + "chartjs-plugin-datalabels/chartjs-plugin-datalabels.min.js",
-                Vendors + "chartjs-plugin-datalabels/chartjs-plugin-datalabels.js")
+                Vendors + "chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js",
+                Vendors + "chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.js")
             .SetVersion(LibManVersions.ChartjsPluginDatalabels);
     }
 
